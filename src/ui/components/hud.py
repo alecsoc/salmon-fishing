d9f36bf93b4f9ddea25b3fac65de1991c6bea6e5
AdrianZambrano1105 <adrianarturozambranorasquide@gmail.com>
2026-02-02 17:34:01 -0400

    Update hud.py

---
+++ src/ui/components/hud.py
@@ -0,0 +1,13 @@
+from settings.py import Settings
+class Menu(): 
+  def __init__(self):
+    fondo=Menu.Jpg.jpg
+    screen.blit(Menu.Jpg,jpg, (0, 0))
+    pygame.display.flip() 
+  def CerrarJuego(self):
+      while True:
+      for event in pygame.event.get()
+        if event.type==pygame.QUIT:
+          break
+class MenuJuego(Menu):
+  pass 

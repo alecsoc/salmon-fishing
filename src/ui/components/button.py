eaaae9a5811e59c4f87c760e8770cd0e60584296
AdrianZambrano1105 <adrianarturozambranorasquide@gmail.com>
2026-02-04 19:26:36 -0400

    Create button.py

---
+++ src/ui/components/button.py
@@ -0,0 +1,11 @@
+import pygame
+class Button:
+  def __init__(self, x, y, width, height):
+    self.rectan = pygame.Rect(x, y, width, height)
+  def draw(self, surface):
+    # TIP: Si alguna vez necesitas ver dónde están, descomenta esto:
+        # pygame.draw.rect(surface, (255, 0, 0), self.rect, width=2)
+    pass
+  def is_clicked(self, mouse_pos):
+    return self.rect.collidepoint(mouse_pos)
+#Clase para los botones de las pantallas

cf2567555925d57cc5a7d2976605fc78f38d9918
AdrianZambrano1105 <adrianarturozambranorasquide@gmail.com>
2026-02-04 18:52:24 -0400

    Update base_screen.py

--- src/ui/base_screen.py
+++ src/ui/base_screen.py
@@ -1,10 +1,17 @@
 from abc import ABC, abstractmethod
 class BaseScreen(ABC):
     @abstractmethod
-    def update(self,events):
+    def update(self,dt):
+        #actualiza la logica matematica 
       pass
     @abstractmethod
-    def draw(self,screen):
+    def draw(self,surface):
+        #dibuja los elementos visuales
       pass
+    @abstractmethod
+    def handle_events(self, events):
+        #recibe la lista de eventos
+        pass
 
 #Intento de menu abstracto para tomar como base para los menus (Inicio,Gameplay,Configuracion)
+
